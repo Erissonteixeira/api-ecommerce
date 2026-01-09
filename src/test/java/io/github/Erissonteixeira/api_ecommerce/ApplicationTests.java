82fa15cdafb0c65835395ad6153c98067c6db10d
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-09 16:04:01 -0300

    refactor(boot): renomeia classe teste para ApplicationTests

--- src/test/java/io/github/Erissonteixeira/api_ecommerce/ApplicationTests.java
+++ src/test/java/io/github/Erissonteixeira/api_ecommerce/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiEcommerceApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
