c05f96d54978de0802daf5ccde3460e66dd1a895
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-09 17:51:00 -0300

    feat(api): adiciona endpoint de health check

---
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/controller/HealthController.java
@@ -0,0 +1,14 @@
+package io.github.Erissonteixeira.api_ecommerce.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("API E-commerce UP");
+    }
+}
