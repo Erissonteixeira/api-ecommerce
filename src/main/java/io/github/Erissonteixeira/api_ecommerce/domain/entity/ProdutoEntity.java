7d82e72671f472062030c7c2ffe5c76feb8e3cce
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-13 15:11:24 -0300

    feat(entity): criar entidade Produto mapeada para a tabela produtos

---
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/entity/ProdutoEntity.java
@@ -0,0 +1,30 @@
+package io.github.Erissonteixeira.api_ecommerce.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "produtos")
+public class ProdutoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @Column(name = "criado_em", nullable = false)
+    private LocalDateTime criadoEm;
+
+    @Column(name = "atualizado_em")
+    private LocalDateTime atualizadoEm;
+}
