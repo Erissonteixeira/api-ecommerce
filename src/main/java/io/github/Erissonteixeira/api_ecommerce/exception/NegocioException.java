d02a18f395ab4fb8539fd44623cd8c3c95d02b52
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-14 14:00:35 -0300

    feat(exception): adiciona exceção de regra de negócio NegocioException

---
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/exception/NegocioException.java
@@ -0,0 +1,8 @@
+package io.github.Erissonteixeira.api_ecommerce.exception;
+
+public class NegocioException extends RuntimeException {
+
+    public NegocioException(String mensagem) {
+        super(mensagem);
+    }
+}
