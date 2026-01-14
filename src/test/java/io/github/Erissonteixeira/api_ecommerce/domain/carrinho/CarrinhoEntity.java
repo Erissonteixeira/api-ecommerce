d92cd67d44f0d4475d6d8cb4cb3d9205d08cbf4d
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-14 15:10:49 -0300

    test(carrinho): adiciona teste unitário para adicionar item no carrinho

---
+++ src/test/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/CarrinhoEntity.java
@@ -0,0 +1,28 @@
+package io.github.Erissonteixeira.api_ecommerce.domain.carrinho;
+
+import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.entity.CarrinhoEntity;
+import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.entity.ItemCarrinhoEntity;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CarrinhoEntityTest {
+
+    @Test
+    void deveAdicionarNovoItemAoCarrinho() {
+        CarrinhoEntity carrinho = new CarrinhoEntity();
+
+        carrinho.adicionarItem(
+                new ItemCarrinhoEntity(
+                        1L,
+                        "Produto A",
+                        new BigDecimal("50.00"),
+                        1
+                )
+        );
+
+        assertEquals(1, carrinho.getItens().size());
+    }
+}
