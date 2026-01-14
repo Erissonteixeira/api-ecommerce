e12dcdf6361333ad431f50ef1aa9dec33642f0d6
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-14 14:03:52 -0300

    feat(exception): cria exceção específica para recurso não encontrado

---
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package io.github.Erissonteixeira.api_ecommerce.exception;
+
+public class RecursoNaoEncontradoException extends NegocioException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
