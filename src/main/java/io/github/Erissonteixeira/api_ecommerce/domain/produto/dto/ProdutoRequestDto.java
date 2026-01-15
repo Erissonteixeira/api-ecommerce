03f72d178acc28c26925a706d4c6af698d972f2f
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-15 11:03:01 -0300

    refactor(dto): reorganizar DTOs para os respectivos domínios

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/dto/ProdutoRequestDto.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/dto/ProdutoRequestDto.java
@@ -1,4 +1,4 @@
-package io.github.Erissonteixeira.api_ecommerce.domain.dto;
+package io.github.Erissonteixeira.api_ecommerce.domain.produto.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/dto/ProdutoResponseDto.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/dto/ProdutoResponseDto.java
@@ -1,4 +1,4 @@
-package io.github.Erissonteixeira.api_ecommerce.domain.dto;
+package io.github.Erissonteixeira.api_ecommerce.domain.produto.dto;
 
 import lombok.Getter;
 import lombok.Setter;
