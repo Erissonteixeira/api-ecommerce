80af8c883128c530a92782781c337eac7634bcf1
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-15 15:09:58 -0300

    test(produto): validar erro ao criar produto com nome inválido

--- src/test/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/controller/ProdutoControllerValidationIT.java
+++ src/test/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/controller/ProdutoControllerValidationIT.java
@@ -1,9 +1,14 @@
 package io.github.Erissonteixeira.api_ecommerce.domain.produto.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -79,4 +84,11 @@ class ProdutoControllerValidationIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    void deveRetornar400_quandoCriarComNomeEmBranco() throws Exception {
+        mockMvc.perform(post("/produtos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JSON_NOME_EM_BRANCO))
+                .andExpect(status().isBadRequest());
+    }
 }
