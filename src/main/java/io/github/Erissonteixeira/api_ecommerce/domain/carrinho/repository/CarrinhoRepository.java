9a347d135758f3de3212ebbccff4b1e0ea2fb5e4
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-15 10:57:53 -0300

    chore(carrinho): adicionar anotação @Repository ao CarrinhoRepository

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/repository/CarrinhoRepository.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/repository/CarrinhoRepository.java
@@ -2,6 +2,8 @@ package io.github.Erissonteixeira.api_ecommerce.domain.carrinho.repository;
 
 import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.entity.CarrinhoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarrinhoRepository extends JpaRepository<CarrinhoEntity, Long> {
 }
