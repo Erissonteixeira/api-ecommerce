c11ec2fda0aa3977108742227d08a9f218e11733
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-15 19:25:47 -0300

    feat(carrinho): adicionar query com fetch join para buscar carrinho com itens

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/repository/CarrinhoRepository.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/repository/CarrinhoRepository.java
@@ -2,8 +2,13 @@ package io.github.Erissonteixeira.api_ecommerce.domain.carrinho.repository;
 
 import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.entity.CarrinhoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CarrinhoRepository extends JpaRepository<CarrinhoEntity, Long> {
+    @Query("select c from CarrinhoEntity c left join fetch c.itens where c.id = :id")
+    Optional<CarrinhoEntity> buscarComItensPorId(Long id);
 }
