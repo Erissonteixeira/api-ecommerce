ecccb9b18fd77708e460dc3b2a8af8e4036aeb96
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-15 11:20:03 -0300

    refactor(mapper): simplificar ProdutoMapper para uso exclusivo via Spring

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/mapper/ProdutoMapper.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/mapper/ProdutoMapper.java
@@ -4,14 +4,9 @@ import io.github.Erissonteixeira.api_ecommerce.domain.produto.dto.ProdutoRequest
 import io.github.Erissonteixeira.api_ecommerce.domain.produto.dto.ProdutoResponseDto;
 import io.github.Erissonteixeira.api_ecommerce.domain.produto.entity.ProdutoEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ProdutoMapper {
-
-    ProdutoMapper INSTANCE = Mappers.getMapper(ProdutoMapper.class);
-
     ProdutoEntity toEntity(ProdutoRequestDto dto);
-
     ProdutoResponseDto toResponse(ProdutoEntity entity);
 }
