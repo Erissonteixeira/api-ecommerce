89c37239e384c2735fcec9ca757c3f06fc72102c
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-15 17:50:13 -0300

    feat(carrinho): adicionar DTO de request para inclusão de itens no carrinho

---
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/dto/CarrinhoAdicionarItemRequestDto.java
@@ -0,0 +1,28 @@
+package io.github.Erissonteixeira.api_ecommerce.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class CarrinhoAdicionarItemRequestDto {
+
+    @NotNull
+    private Long produtoId;
+
+    @NotBlank
+    private String nomeProduto;
+
+    @NotNull
+    @Positive
+    private BigDecimal preco;
+
+    @NotNull
+    @Positive
+    private Integer quantidade;
+}
