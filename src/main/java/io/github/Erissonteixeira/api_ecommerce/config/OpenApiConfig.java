7c6fcd434e13a4f8c635f6bfe51e4adcf4247da2
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-16 15:27:58 -0300

    chore(openapi): refatorar configuração do OpenAPI com metadados e agrupamento de endpoints

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/config/OpenApiConfig.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/config/OpenApiConfig.java
@@ -1,16 +1,47 @@
 package io.github.Erissonteixeira.api_ecommerce.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "API E-commerce",
-                version = "v1",
-                description = "API do projeto E-commerce"
-        )
-)
 public class OpenApiConfig {
+
+        @Bean
+        public OpenAPI openAPI() {
+                return new OpenAPI()
+                        .info(new Info()
+                                .title("API E-commerce")
+                                .description("API REST para gestão de produtos e carrinho de compras.")
+                                .version("v1")
+                                .contact(new Contact()
+                                        .name("Erisson Teixeira")
+                                        .email("seu-email@exemplo.com")
+                                )
+                                .license(new License()
+                                        .name("MIT")
+                                        .url("https://opensource.org/licenses/MIT")
+                                )
+                        );
+        }
+
+        @Bean
+        public GroupedOpenApi produtosApi() {
+                return GroupedOpenApi.builder()
+                        .group("Produtos")
+                        .pathsToMatch("/produtos/**")
+                        .build();
+        }
+
+        @Bean
+        public GroupedOpenApi carrinhosApi() {
+                return GroupedOpenApi.builder()
+                        .group("Carrinhos")
+                        .pathsToMatch("/carrinhos/**")
+                        .build();
+        }
 }
