411a61b2810bc1bf8501bc4a07d0cbd94c5800f7
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-21 20:14:03 -0300

    refactor: atualizar imports do CarrinhoControllerValidationIT após mover CarrinhoAdicionarItemRequestDto para domain.carrinho.dto

--- src/test/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/controller/CarrinhoControllerValidationIT.java
+++ src/test/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/controller/CarrinhoControllerValidationIT.java
@@ -1,7 +1,7 @@
 package io.github.Erissonteixeira.api_ecommerce.domain.carrinho.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.Erissonteixeira.api_ecommerce.domain.dto.CarrinhoAdicionarItemRequestDto;
+import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.dto.CarrinhoAdicionarItemRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
