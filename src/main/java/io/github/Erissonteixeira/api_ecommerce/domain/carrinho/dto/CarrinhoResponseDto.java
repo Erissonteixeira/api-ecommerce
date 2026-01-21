cdd4ce0ac0df6465ce37c1b961a79404d9e67897
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-21 20:14:39 -0300

    feat: adicionar CarrinhoResponseDto para resposta do carrinho com itens e total

---
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/dto/CarrinhoResponseDto.java
@@ -0,0 +1,18 @@
+package io.github.Erissonteixeira.api_ecommerce.domain.carrinho.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class CarrinhoResponseDto {
+    private Long id;
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+    private List<CarrinhoItemResponseDto> itens;
+    private BigDecimal total;
+}
