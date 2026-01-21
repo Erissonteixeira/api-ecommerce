4d40e93eb380ee1c885ab44e033990ddba6df731
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-01-21 20:12:25 -0300

    refactor: ajustar mapeamentos do CarrinhoMapper após refatoração de métodos e pacotes

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/mapper/CarrinhoMapper.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/mapper/CarrinhoMapper.java
@@ -1,18 +1,19 @@
 package io.github.Erissonteixeira.api_ecommerce.domain.carrinho.mapper;
 
-import io.github.Erissonteixeira.api_ecommerce.domain.dto.CarrinhoItemResponseDto;
-import io.github.Erissonteixeira.api_ecommerce.domain.dto.CarrinhoResponseDto;
+import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.dto.CarrinhoItemResponseDto;
+import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.dto.CarrinhoResponseDto;
 import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.entity.CarrinhoEntity;
 import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.entity.ItemCarrinhoEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CarrinhoMapper {
 
-    @Mapping(target = "total", expression = "java(entity.calcularTotal())")
-    CarrinhoResponseDto toResponse(CarrinhoEntity entity);
+    CarrinhoResponseDto toResponseDto(CarrinhoEntity entity);
 
-    @Mapping(target = "subtotal", expression = "java(item.calcularSubtotal())")
-    CarrinhoItemResponseDto toItemResponse(ItemCarrinhoEntity item);
+    CarrinhoItemResponseDto toItemResponseDto(ItemCarrinhoEntity entity);
+
+    List<CarrinhoItemResponseDto> toItemResponseDtoList(List<ItemCarrinhoEntity> itens);
 }
