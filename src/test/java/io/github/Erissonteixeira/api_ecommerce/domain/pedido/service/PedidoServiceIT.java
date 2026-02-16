4948f9d069435d361d3008cc8c81a1936b54a0ec
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-02-16 15:32:19 -0300

    test(pedido): ajustar PedidoServiceIT com setup e novas asserções

--- src/test/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/service/PedidoServiceIT.java
+++ src/test/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/service/PedidoServiceIT.java
@@ -5,6 +5,7 @@ import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.entity.ItemCarrin
 import io.github.Erissonteixeira.api_ecommerce.domain.carrinho.repository.CarrinhoRepository;
 import io.github.Erissonteixeira.api_ecommerce.domain.pedido.entity.PedidoEntity;
 import io.github.Erissonteixeira.api_ecommerce.domain.pedido.repository.PedidoRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,8 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -28,23 +28,31 @@ class PedidoServiceIT {
     @Autowired
     private PedidoRepository pedidoRepository;
 
+    @BeforeEach
+    void setup() {
+        pedidoRepository.deleteAll();
+        carrinhoRepository.deleteAll();
+    }
+
     @Test
     void deveCriarPedidoPersistidoAPartirDeCarrinhoComItens() {
 
         CarrinhoEntity carrinho = new CarrinhoEntity();
-        carrinho.adicionarItem(new ItemCarrinhoEntity(10L, "mouse gamer", new BigDecimal("150.00"), 2)); // 300
-        carrinho.adicionarItem(new ItemCarrinhoEntity(11L, "teclado", new BigDecimal("150.00"), 2));    // 300
+        carrinho.adicionarItem(new ItemCarrinhoEntity(10L, "mouse gamer", new BigDecimal("150.00"), 2));
+        carrinho.adicionarItem(new ItemCarrinhoEntity(11L, "teclado", new BigDecimal("150.00"), 2));
 
-        CarrinhoEntity salvo = carrinhoRepository.save(carrinho);
+        CarrinhoEntity salvo = carrinhoRepository.saveAndFlush(carrinho);
 
         PedidoEntity pedido = pedidoService.criarPedidoDoCarrinho(salvo.getId());
 
         assertNotNull(pedido.getId());
         assertEquals(2, pedido.getItens().size());
-        assertEquals(new BigDecimal("600.00"), pedido.getTotal());
+        assertEquals(new BigDecimal("600.00"), pedido.getTotal().setScale(2));
+        assertEquals("CRIADO", pedido.getStatus().name());
+        assertNotNull(pedido.getCriadoEm());
 
         PedidoEntity encontrado = pedidoRepository.findById(pedido.getId()).orElseThrow();
-        assertEquals(new BigDecimal("600.00"), encontrado.getTotal());
+        assertEquals(new BigDecimal("600.00"), encontrado.getTotal().setScale(2));
         assertEquals(2, encontrado.getItens().size());
     }
 }
