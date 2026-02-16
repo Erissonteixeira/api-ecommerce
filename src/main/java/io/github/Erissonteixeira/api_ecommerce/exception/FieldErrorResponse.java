a3852b451199cae1b066901900f2cccd71569e72
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-02-16 16:28:58 -0300

    refactor(exception): adicionar anotações @Schema ao FieldErrorResponse

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/exception/FieldErrorResponse.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/exception/FieldErrorResponse.java
@@ -1,11 +1,17 @@
 package io.github.Erissonteixeira.api_ecommerce.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@Schema(name = "FieldError")
 public class FieldErrorResponse {
+
+    @Schema(example = "nome")
     private String field;
+
+    @Schema(example = "não deve estar em branco")
     private String message;
 }
