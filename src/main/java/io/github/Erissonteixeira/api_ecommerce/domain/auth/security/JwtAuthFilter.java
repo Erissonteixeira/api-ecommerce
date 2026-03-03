557ba82e7642d31ed4811da429fbccd13d859f74
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-03 18:15:55 -0300

    feat(auth): adicionar JwtAuthFilter para autenticação via token JWT

---
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/auth/security/JwtAuthFilter.java
@@ -0,0 +1,46 @@
+package io.github.Erissonteixeira.api_ecommerce.domain.auth.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            if (jwtService.tokenValido(token) && SecurityContextHolder.getContext().getAuthentication() == null) {
+                String email = jwtService.extrairEmail(token);
+
+                var auth = new UsernamePasswordAuthenticationToken(email, null, List.of());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
