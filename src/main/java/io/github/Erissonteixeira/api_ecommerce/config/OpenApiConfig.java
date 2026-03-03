843ecb81af277ef085cfc7dbff3a4846d9df1bd8
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-03 20:05:08 -0300

    refactor(openapi): adicionar esquema bearer JWT e security requirement no swagger

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/config/OpenApiConfig.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package io.github.Erissonteixeira.api_ecommerce.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +18,12 @@ public class OpenApiConfig {
         private static final String GROUP_PRODUTOS = "Produtos";
         private static final String GROUP_CARRINHOS = "Carrinhos";
 
+        private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
         @Bean
         public OpenAPI openAPI() {
                 return new OpenAPI()
+
                         .info(new Info()
                                 .title("API E-commerce")
                                 .description("API REST para gestão de usuários, produtos, carrinho e pedidos.")
@@ -29,6 +35,19 @@ public class OpenApiConfig {
                                 .license(new License()
                                         .name("MIT")
                                         .url("https://opensource.org/licenses/MIT")
+                                )
+                        )
+
+                        .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+
+                        .components(new Components()
+                                .addSecuritySchemes(
+                                        SECURITY_SCHEME_NAME,
+                                        new SecurityScheme()
+                                                .name("Authorization")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
                                 )
                         );
         }
