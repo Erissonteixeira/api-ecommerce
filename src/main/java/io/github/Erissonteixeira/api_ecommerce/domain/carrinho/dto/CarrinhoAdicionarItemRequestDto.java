5b66a4b6c8711505f7e06376890e2b029404a83a
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-04 19:18:25 -0300

    refactor(carrinho): simplificar CarrinhoAdicionarItemRequestDto removendo nomeProduto e preco

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/dto/CarrinhoAdicionarItemRequestDto.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/carrinho/dto/CarrinhoAdicionarItemRequestDto.java
@@ -1,12 +1,9 @@
 package io.github.Erissonteixeira.api_ecommerce.domain.carrinho.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 
 @Getter
 @Setter
@@ -14,13 +11,6 @@ public class CarrinhoAdicionarItemRequestDto {
 
     @NotNull
     private Long produtoId;
-
-    @NotBlank
-    private String nomeProduto;
-
-    @NotNull
-    @Positive
-    private BigDecimal preco;
 
     @NotNull
     @Positive
