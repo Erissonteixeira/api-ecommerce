c05a87de291820bd95ec083d6c68a3e6a400a441
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-04 20:50:10 -0300

    refactor(pedido): alterar assinatura de criarPedidoDoCarrinho para receber email do usuário

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/service/PedidoService.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/service/PedidoService.java
@@ -3,5 +3,7 @@ package io.github.Erissonteixeira.api_ecommerce.domain.pedido.service;
 import io.github.Erissonteixeira.api_ecommerce.domain.pedido.entity.PedidoEntity;
 
 public interface PedidoService {
-    PedidoEntity criarPedidoDoCarrinho(Long carrinhoId);
+
+    PedidoEntity criarPedidoDoCarrinho(String email);
+
 }
