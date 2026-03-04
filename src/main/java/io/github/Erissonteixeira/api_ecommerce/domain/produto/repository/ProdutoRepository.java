c0202cd84375e3d259ebca7cbab4257352a372ad
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-04 17:36:05 -0300

    refactor(produto): ajustar consultas do ProdutoRepository para findAllByAtivoTrue e findByIdAndAtivoTrue

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/repository/ProdutoRepository.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/produto/repository/ProdutoRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoEntity, Long> {
 
-    List<ProdutoEntity> findByAtivoTrue();
+    List<ProdutoEntity> findAllByAtivoTrue();
 
+    Optional<ProdutoEntity> findByIdAndAtivoTrue(Long id);
 }
