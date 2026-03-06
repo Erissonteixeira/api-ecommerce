bca0f5df0b4c5413fdffd37753530075e23e261d
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-06 15:54:00 -0300

    feat(pedido-service): adicionar métodos para consultar pedidos do usuário

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/service/PedidoService.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/service/PedidoService.java
@@ -2,8 +2,13 @@ package io.github.Erissonteixeira.api_ecommerce.domain.pedido.service;
 
 import io.github.Erissonteixeira.api_ecommerce.domain.pedido.entity.PedidoEntity;
 
+import java.util.List;
+
 public interface PedidoService {
 
     PedidoEntity criarPedidoDoCarrinho(String email);
 
+    List<PedidoEntity> listarMeusPedidos(String email);
+
+    PedidoEntity buscarMeuPedidoPorId(String email, Long pedidoId);
 }
