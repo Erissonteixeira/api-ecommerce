aaa75c50eced165fc3c9aac822ffcafdac8d30f9
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-06 15:38:23 -0300

    feat(pedido-repository): adicionar consultas por usuário

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/repository/PedidoRepository.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/repository/PedidoRepository.java
@@ -3,5 +3,12 @@ package io.github.Erissonteixeira.api_ecommerce.domain.pedido.repository;
 import io.github.Erissonteixeira.api_ecommerce.domain.pedido.entity.PedidoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PedidoRepository extends JpaRepository<PedidoEntity, Long> {
+
+    List<PedidoEntity> findAllByUsuarioIdOrderByCriadoEmDesc(Long usuarioId);
+
+    Optional<PedidoEntity> findByIdAndUsuarioId(Long id, Long usuarioId);
 }
