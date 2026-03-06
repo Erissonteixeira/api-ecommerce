9e7a1f32797b78c04b7b20b2db6778484033405e
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-06 19:40:07 -0300

    refactor(pedido-dto): reorganizar getters e setters em PedidoItemResponseDto

--- src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/dto/PedidoItemResponseDto.java
+++ src/main/java/io/github/Erissonteixeira/api_ecommerce/domain/pedido/dto/PedidoItemResponseDto.java
@@ -18,44 +18,44 @@ public class PedidoItemResponseDto {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getProdutoId() {
         return produtoId;
-    }
-
-    public void setProdutoId(Long produtoId) {
-        this.produtoId = produtoId;
     }
 
     public String getNomeProduto() {
         return nomeProduto;
     }
 
-    public void setNomeProduto(String nomeProduto) {
-        this.nomeProduto = nomeProduto;
-    }
-
     public BigDecimal getPrecoUnitario() {
         return precoUnitario;
-    }
-
-    public void setPrecoUnitario(BigDecimal precoUnitario) {
-        this.precoUnitario = precoUnitario;
     }
 
     public Integer getQuantidade() {
         return quantidade;
     }
 
-    public void setQuantidade(Integer quantidade) {
-        this.quantidade = quantidade;
-    }
-
     public BigDecimal getSubtotal() {
         return subtotal;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setProdutoId(Long produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
     }
 
     public void setSubtotal(BigDecimal subtotal) {
